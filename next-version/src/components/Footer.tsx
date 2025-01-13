2cccef0d7bb64396079e190bbe3f5636005cfe1b
whdjh <whdjh98@naver.com>
2025-01-13 17:50:46 +0900

    feat: 설명 수정

--- next-version/src/components/Footer.tsx
+++ next-version/src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <>
       <div className={styles.container}>
-        "온라인 피아노를 사용하면 웹 브라우저에서 피아노 키보드의 거의 모든 악기를 연주할 수 있습니다.
-        <br />노트북이나 데스크탑이 있다면 연주할 수 있는 온라인 피아노입니다.
+        "온라인 피아노를 사용하면 웹 브라우저에서 피아노 키보드를 연주할 수 있습니다.
+        <br />웹페이지에 접속된다면 연주할 수 있는 온라인 피아노입니다.
         <br />노트북 키보드를 사용하여 연주할 수 있는 피아노가 제공됩니다."
       </div>
       <div className={styles.container}>
