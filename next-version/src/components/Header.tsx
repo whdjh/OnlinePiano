2e08d976e5138c02c5f3452c33f88997b0c7f648
whdjh <whdjh98@naver.com>
2025-01-13 17:30:35 +0900

    feat: svgr 설치

--- next-version/src/components/Header.tsx
+++ next-version/src/components/Header.tsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
+import Logo from '../../public/Logo.svg';
 
 export default function Header() {
   return (
     <>
       <div className={styles.container}>
+        <Logo />
         <p className={styles.title}>가상 피아노</p>
       </div>
       <hr className={styles.line} />
