7dea52bfb62f61f5f29731da4b463630326bfead
whdjh <whdjh98@naver.com>
2025-01-13 22:31:53 +0900

    feat:모바일 퍼스트 건반 레이아웃 구성 완료

--- next-version/src/components/Main.tsx
+++ next-version/src/components/Main.tsx
@@ -2,6 +2,36 @@ import styles from './Main.module.css';
 
 export default function Main() {
   return (
-    <div>메인 부분입니다.</div>
+    <>
+      <div className={styles.piano}>
+        <button className={`${styles.key} ${styles.white}`}>도</button>
+        <button className={`${styles.key} ${styles.black}`}>레b</button>
+        <button className={`${styles.key} ${styles.white}`}>레</button>
+        <button className={`${styles.key} ${styles.black}`}>미b</button>
+        <button className={`${styles.key} ${styles.white}`}>미</button>
+        <button className={`${styles.key} ${styles.white}`}>파</button>
+        <button className={`${styles.key} ${styles.black}`}>솔b</button>
+        <button className={`${styles.key} ${styles.white}`}>솔</button>
+        <button className={`${styles.key} ${styles.black}`}>라b</button>
+        <button className={`${styles.key} ${styles.white}`}>라</button>
+        <button className={`${styles.key} ${styles.black}`}>시b</button>
+        <button className={`${styles.key} ${styles.white}`}>시</button>
+        
+      </div>
+      <div className={styles.piano}>
+        <button className={`${styles.key} ${styles.white}`}>도</button>
+        <button className={`${styles.key} ${styles.black}`}>레b</button>
+        <button className={`${styles.key} ${styles.white}`}>레</button>
+        <button className={`${styles.key} ${styles.black}`}>미b</button>
+        <button className={`${styles.key} ${styles.white}`}>미</button>
+        <button className={`${styles.key} ${styles.white}`}>파</button>
+        <button className={`${styles.key} ${styles.black}`}>솔b</button>
+        <button className={`${styles.key} ${styles.white}`}>솔</button>
+        <button className={`${styles.key} ${styles.black}`}>라b</button>
+        <button className={`${styles.key} ${styles.white}`}>라</button>
+        <button className={`${styles.key} ${styles.black}`}>시b</button>
+        <button className={`${styles.key} ${styles.white}`}>시</button>
+      </div>
+    </>
   );
 }
