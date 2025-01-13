1a9b79c4c19e8838e7f3682ce0054fad92e07e80
whdjh <whdjh98@naver.com>
2025-01-13 17:18:58 +0900

    feat: SEO맞게 페이지 구성 완료

---
+++ next-version/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div>푸터 부분입니다.</div>
+  );
+}
---
+++ next-version/src/components/Main.tsx
@@ -0,0 +1,7 @@
+import styles from './Main.module.css';
+
+export default function Main() {
+  return (
+    <div>메인 부분입니다.</div>
+  );
+}
